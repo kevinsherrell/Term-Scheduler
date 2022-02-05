a9cc9c6bee8cd81e99b9ad0b078436981b82bf63
kevinsherrell <k.sherrell.1028@gmail.com>
2022-02-04 23:12:38 -0700

    added listeners to buttons

--- app/src/main/java/com/c196/TermScheduler/UI/Term/TermAdd.java
+++ app/src/main/java/com/c196/TermScheduler/UI/Term/TermAdd.java
@@ -3,15 +3,36 @@ package com.c196.TermScheduler.UI.Term;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.c196.TermScheduler.R;
 
 public class TermAdd extends AppCompatActivity {
-
+    public String TAG = "TermAdd";
+    // id termAddTitleInput
+    public EditText termAddTitleInput;
+    // id termAddStartButton and termAddSubmit
+    public Button termAddStartButton, termAddSubmit;
+    // id termAddDateText
+    public TextView termAddDateText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_add);
+
+        termAddStartButton = findViewById(R.id.termAddStartButton);
+        termAddSubmit = findViewById(R.id.termAddSubmit);
+        termAddStartButton.setOnClickListener(view->{
+            Log.d(TAG, "onCreate: onClickDate");
+        });
+
+        termAddSubmit.setOnClickListener(view->{
+            Log.d(TAG, "onCreate: onClickSubmit");
+        });
     }
 
 }
