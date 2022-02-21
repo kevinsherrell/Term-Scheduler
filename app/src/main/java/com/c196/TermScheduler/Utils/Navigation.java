e5e8f8c7639db7fd7d9e3aa82ed9ec2cf9d6b826
kevinsherrell <k.sherrell.1028@gmail.com>
2022-02-20 21:31:15 -0700

    create static navigate method

--- app/src/main/java/com/c196/TermScheduler/Utils/Navigation.java
+++ app/src/main/java/com/c196/TermScheduler/Utils/Navigation.java
@@ -1,4 +1,31 @@
 package com.c196.TermScheduler.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
 public class Navigation {
+
+    public static boolean navigate(Context context, Class cls) throws Exception {
+        try {
+            Intent intent = new Intent(context, cls);
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean navigate(Context context, Class cls, Bundle bundle) throws Exception {
+        try {
+            Intent intent = new Intent(context, cls);
+            intent.putExtras(bundle);
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
